4a82eb518a60b7221348ee16502402c53ebc7e1e
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2021-04-30 12:32:34 +0700

    addiconmenu

--- lib/page/menu.dart
+++ lib/page/menu.dart
@@ -11,26 +11,24 @@ class Menu_Page extends StatefulWidget {
 }
 
 class _Menu_PageState extends State<Menu_Page> {
-  String nameclass = 'Myinfo';
+  String nameclass = 'ข้อมูลของฉัน';
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(left: 90),
-          child: Text(nameclass),
-        ),
+        centerTitle: true,
+        title: Text(nameclass),
       ),
       body:
           // nameclass == "login"? Login_Page():
-          nameclass == "History"
+          nameclass == "ประวัติการรักษา"
               ? History_Page()
-              : nameclass == "Myinfo"
+              : nameclass == "ข้อมูลของฉัน"
                   ? Myinfo_Page()
-                  : nameclass == "Appointment"
+                  : nameclass == "นัดหมาย"
                       ? Appointment_Page()
-                      : nameclass == 'Queue'
+                      : nameclass == 'คิวของฉัน'
                           ? Queue_Page()
                           : Login_Page(),
       drawer: Container(
@@ -39,62 +37,107 @@ class _Menu_PageState extends State<Menu_Page> {
           child: ListView(
             children: <Widget>[
               UserAccountsDrawerHeader(
-                accountName: Text("Ashish Rawat"),
-                accountEmail: Text("ashishrawat2911@gmail.com"),
+                accountName: Text("Anucha Ar'art"),
+                accountEmail: Text("anucha.su.61@ubu.ac.th"),
                 currentAccountPicture: CircleAvatar(
-                  backgroundColor:
-                      Theme.of(context).platform == TargetPlatform.iOS
-                          ? Colors.blue
-                          : Colors.white,
-                  child: Text(
-                    "B",
-                    style: TextStyle(fontSize: 40.0),
-                  ),
+                  backgroundImage: NetworkImage(
+                      'https://scontent.fbkk10-1.fna.fbcdn.net/v/t1.6435-9/48382402_1468943199904553_3400320523001921536_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeHY85MVTqs1ShEH-UPqxBs-Af2gYIqEY5AB_aBgioRjkKoJDOtnKEqTvne5xFU3zcTxhMnAeXV0tRC5C2NojcMC&_nc_ohc=vfoZGIsrVfEAX_W28m6&_nc_ht=scontent.fbkk10-1.fna&oh=a6e4d6829e3418e438edd12f71971e07&oe=60B07736'),
                 ),
               ),
               ListTile(
                 onTap: () {
                   setState(() {
-                    nameclass = "Myinfo";
+                    nameclass = "ข้อมูลของฉัน";
                   });
 
                   Navigator.of(context).pop();
                 },
-                title: Text("ข้อมูลของฉัน"),
+                leading: Image.network(
+                  'https://cdn.discordapp.com/attachments/834620062090133543/836836380675145758/969312.png',
+                  width: 30,
+                  height: 30,
+                ),
+                title: Text(
+                  "ข้อมูลของฉัน",
+                  style: TextStyle(fontSize: 18),
+                ),
                 trailing: Icon(Icons.arrow_forward),
               ),
               ListTile(
                 onTap: () {
                   setState(() {
-                    nameclass = "History";
+                    nameclass = "ประวัติการรักษา";
                   });
 
                   Navigator.of(context).pop();
                 },
-                title: Text("ประวัติการรักษา"),
+                leading: Image.network(
+                  'https://media.discordapp.net/attachments/834620062090133543/836834745303826472/3601157.png',
+                  width: 30,
+                  height: 30,
+                ),
+                title: Text(
+                  "ประวัติการรักษา",
+                  style: TextStyle(fontSize: 18),
+                ),
                 trailing: Icon(Icons.arrow_forward),
               ),
               ListTile(
                 onTap: () {
                   setState(() {
-                    nameclass = "Appointment";
+                    nameclass = "นัดหมาย";
                   });
 
                   Navigator.of(context).pop();
                 },
-                title: Text("นัดหมาย"),
+                leading: Image.network(
+                  'https://media.discordapp.net/attachments/834620062090133543/836835672051417128/3652191.png',
+                  width: 30,
+                  height: 30,
+                ),
+                title: Text(
+                  "นัดหมาย",
+                  style: TextStyle(fontSize: 18),
+                ),
                 trailing: Icon(Icons.arrow_forward),
               ),
               ListTile(
-                  onTap: () {
+                onTap: () {
                   setState(() {
-                    nameclass = "Queue";
+                    nameclass = "คิวของฉัน";
                   });
 
                   Navigator.of(context).pop();
                 },
-                title: Text("คิวของฉัน"),
+                leading: Image.network(
+                  'https://media.discordapp.net/attachments/834620062090133543/836834856531918878/3113776.png',
+                  width: 30,
+                  height: 30,
+                ),
+                title: Text(
+                  "คิวของฉัน",
+                  style: TextStyle(fontSize: 18),
+                ),
                 trailing: Icon(Icons.arrow_forward),
+              ),
+              ListTile(
+                onTap: () {
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, "/login", (route) => false);
+                },
+                leading: Image.network(
+                  'https://media.discordapp.net/attachments/834620062090133543/837178484370571274/logout.png',
+                  width: 30,
+                  height: 30,
+                ),
+                title: Text(
+                  "ออกจากระบบ",
+                  style: TextStyle(fontSize: 18, color: Colors.redAccent[700]),
+                ),
+                trailing: Icon(
+                  Icons.arrow_forward,
+                  color: Colors.redAccent[700],
+                ),
               ),
             ],
           ),
