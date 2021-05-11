0d1aa76402725a3dce1c7cb8a91930775a42f637
WineXerox <tanapol.ta.61@ubu.ac.th>
2021-05-11 18:12:54 +0700

    QueuePage

--- lib/page/queue.dart
+++ lib/page/queue.dart
@@ -19,9 +19,8 @@ class _Queue_PageState extends State<Queue_Page> {
             future: Network.fetchQueue(),
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return Container(
-                  child: _listInfo(data :snapshot.data),
-                );
+                return 
+                  _listInfo(data :snapshot.data);
               } else if (snapshot.hasError) {
                 return Text('${snapshot.error}');
               }
@@ -33,6 +32,62 @@ class _Queue_PageState extends State<Queue_Page> {
     );
   }
   Widget _listInfo({List<QueueInfo> data}){
-    return Container(child: Text(data[1].unitName),);
+    return 
+      // padding: const EdgeInsets.all(8.0),
+      Container(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children :[
+                  Image.network(
+                    'https://media.discordapp.net/attachments/834620062090133543/836834856531918878/3113776.png',
+                    width: 30,
+                    height: 30,
+                ),
+                Text('คิวของฉัน',style: TextStyle(fontSize: 28)),
+              ],
+            ), 
+            Text('รหัสคิว',style: TextStyle(fontSize: 24)),
+            Text(data[0].qCode,style: TextStyle(fontSize: 18),),
+            Text('VN : ',style: TextStyle(fontSize: 24,color: Colors.blue)),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+              Text('จุดบริการ',style: TextStyle(fontSize: 18,color: Colors.blue)),
+              Text('สถานะ',style: TextStyle(fontSize: 18,color: Colors.blue)),
+              ],
+            ),
+
+            Divider(thickness: 2,color: Colors.blue),
+            // Text(data),
+            // Text(data),
+
+            Text('สถานที่รับบริการ : ',style: TextStyle(fontSize: 20)),
+            Text('การเตรียมตัวก่อนพบแพทย์ : ',style: TextStyle(fontSize: 20)),
+            Text('สิ่งที่ต้องเตรียม : ',style: TextStyle(fontSize: 20)),
+            Text('จำนวนคิวที่เหลือ : ',style: TextStyle(fontSize: 20)),
+            Text('เวลาที่จะถึงคิวของท่าน : ',style: TextStyle(fontSize: 20)),
+
+            Text('ประวัติคิวทั้งหมด',style: TextStyle(fontSize: 24)),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+              Text('วันที่',style: TextStyle(fontSize: 18,color: Colors.blue)),
+              Text('เวลา',style: TextStyle(fontSize: 18,color: Colors.blue)),
+              Text('รหัสคิว',style: TextStyle(fontSize: 18,color: Colors.blue)),
+              Text('แผนก',style: TextStyle(fontSize: 18,color: Colors.blue)),
+              Text('ดูเพิ่ม',style: TextStyle(fontSize: 18,color: Colors.blue)),
+              ],
+            ),
+            // Text(data)
+            Divider(thickness: 2,color: Colors.blue),
+            
+          ],
+        ),
+      );
   }
 }
